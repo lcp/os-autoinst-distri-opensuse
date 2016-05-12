b2f94185baf27b0573c59175ddeb604c3ab7429c
Gary Lin <glin@suse.com>
2016-05-12 16:55:46 +0800

    Add the script to enroll the devel key into db

--- tests/moktest/enroll_devel_key.pm
+++ tests/moktest/enroll_devel_key.pm
@@ -1,19 +1,23 @@
 use base "consoletest";
 use testapi;
+use utils;
 
 sub run() {
     my $self = shift;
     my $devel_key = 'data/shim-devel.der';
-    my $new_shim = 'data/shim-opensuse.efi';
 
-    script_sudo("cp -f $devel_key /boot/efi/");
-    script_sudo("cp -f $new_shim /boot/efi/EFI/opensuse/");
-    script_sudo("efibootmgr -c -d /dev/vda -l \"\\EFI\\opensuse\\shim-opensuse.efi\" -L \"test\"");
+    select_console 'user-console';
+    assert_script_run("curl -L -v " . autoinst_url('data/shim-devel.der') . " > shim-key.der");
+    # TODO install the new shim
+    script_sudo("cp -f shim-key.der /boot/efi/EFI/shim-key.der");
     # boot to the firmware UI
-    script_sudo("echo -ne \"\\x07\\x00\\x00\\x00\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\" > /sys/firmware/efi/efivars/OsIndications-8be4df61-93ca-11d2-aa0d-00e098032b8c");
-    script_sudo("reboot");
+    assert_script_sudo("echo -ne \"\\x07\\x00\\x00\\x00\\x01\\x00\\x00\\x00\\x00\\x00\\x00\\x00\" > /sys/firmware/efi/efivars/OsIndications-8be4df61-93ca-11d2-aa0d-00e098032b8c");
 
-    wait_idle;
+    become_root;
+    type_string "reboot\n";
+    reset_consoles;
+
+    assert_screen("ovmf-menu");
 
     # Go to Device Manager
     send_key "down";
@@ -21,116 +25,105 @@ sub run() {
     send_key "down";
     send_key "ret";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-device-manager");
 
     # Into Secure Boot Configuration
     send_key "ret";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-secure-boot-conf");
 
     send_key "down";
     send_key "down";
+    send_key "down";
     send_key "ret";
+
+    assert_screen("ovmf-change-sb");
+
     # Set to Custom mode
     send_key "down";
     send_key "ret";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-customized-sb");
 
     # Custom Secure Boot Options
     send_key "down";
     send_key "ret";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-sb-option");
 
     # DB Options
     send_key "down";
     send_key "down";
-    sleep 3;
-    save_screenshot;
+    send_key "down";
+
+    assert_screen("ovmf-sb-option-db");
+
     send_key "ret";
-    sleep 3;
-    save_screenshot;
+
+    assert_screen("ovmf-db-enroll");
 
     # Enroll Signature
     send_key "ret";
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-enroll-sig");
+
     # Enroll Signature Using File
     send_key "ret";
-    sleep 3;
+    wait_idle;
     save_screenshot;
     # Choose harddisk
-    send_key "down";
-    send_key "ret";
-    send_key "down";
-    send_key "down";
-    sleep 3;
-    save_screenshot;
-    # Choose shim-devel.der
+    #send_key "down";
     send_key "ret";
 
-    sleep 3;
+    assert_screen("ovmf-file-explorer-efi");
+    # Choose <EFI>
+    send_key "ret";
+
+    wait_idle;
+    save_screenshot;
+
+    # Choose <opensuse>
+    send_key "down";
+    send_key "down";
+    send_key "down";
+    send_key "down";
+    assert_screen("ovmf-choose-shim-key");
+    # Choose shim-key.der
+    send_key "ret";
+
+    wait_idle;
     save_screenshot;
 
     # Commit changes
     send_key "down";
     send_key "down";
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-commit-sig");
     send_key "ret";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-sb-option-db");
 
     # Back to the main menu
     send_key "esc";
     send_key "esc";
     send_key "esc";
 
-    sleep 3;
-    save_screenshot;
+    assert_screen("ovmf-menu");
 
     # continue
     send_key "ret";
 
-    assert_screen "grub2", 15;
-    send_key "ret";
-
-    if ( get_var("NOAUTOLOGIN") ) {
-        assert_screen 'displaymanager', 300;
-    }
-    else {
-        assert_screen 'generic-desktop', 300;
-    }
-
+    # XXX It seems the serial output from OVMF could cause problem
+    # later. Matching the GRUB message could work around the issue.
+    wait_serial("Welcome to GRUB!");
     save_screenshot;
 
-    # verify there is a text console on tty1
-    send_key "ctrl-alt-f1";
-    assert_screen "tty1-selected", 15;
+    wait_boot;
+    select_console 'user-console';
 
-    # init
-    # log into text console
-    send_key "ctrl-alt-f4";
-    # we need to wait more than five seconds here to pass the idle timeout in
-    # case the system is still booting (https://bugzilla.novell.com/show_bug.cgi?id=895602)
-    assert_screen "tty4-selected", 10;
-    assert_screen "text-login", 10;
-    type_string "$username\n";
-    sleep 2;
-    assert_screen "password-prompt", 10;
-    type_password;
-    type_string "\n";
-    sleep 3;
-    type_string "PS1=\$\n";    # set constant shell promt
-    sleep 1;
+    assert_script_sudo "chown $username /dev/$serialdev";
+}
 
-    script_sudo "chown $username /dev/$serialdev";
+sub test_flags() {
+    return { 'important' => 1, 'milestone' => 1, 'fatal' => 1 };
 }
 
 1;
