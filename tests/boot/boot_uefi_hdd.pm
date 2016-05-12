65b1176f85b53319dfc18a3c621db053808accc8
Gary Lin <glin@suse.com>
2016-05-12 17:05:40 +0800

    Add the script to boot into HDD in UEFI

---
+++ tests/boot/boot_uefi_hdd.pm
@@ -0,0 +1,69 @@
+use base "basetest";
+use testapi;
+
+sub run() {
+    my $self = shift;
+
+    # Remove the DVD
+    assert_screen "bootloader-grub2", 15;
+    eject_cd;
+    power("reset");
+
+    assert_screen("ovmf-start");
+
+    send_key "esc";
+
+    assert_screen("ovmf-menu");
+
+    send_key "down";
+    send_key "down";
+    send_key "down";
+    send_key "down";
+
+    # Choose Boot Maintenance Manager
+    assert_screen("ovmf-boot-maintenance");
+    send_key "ret";
+
+    # Boot Maintenance Manager
+    assert_screen("ovmf-boot-maintenance-menu");
+    send_key "down";
+    send_key "down";
+    send_key "down";
+    
+    # Choose Boot From File
+    wait_idle;
+    save_screenshot;
+
+    send_key "ret";
+
+    # File Explorer
+    # HDD
+    wait_idle;
+    save_screenshot;
+
+    send_key "ret";
+
+    # <efi>
+    assert_screen("ovmf-file-efi");
+    send_key "ret";
+
+    # <SuSE>
+    assert_screen("ovmf-file-suse");
+    send_key "down";
+    send_key "down";
+    send_key "ret";
+
+    # shim.efi
+    assert_screen("ovmf-file-bootloaders");
+    send_key "down";
+    send_key "ret";
+
+    save_screenshot;
+}
+
+sub test_flags() {
+    return { 'important' => 1, 'milestone' => 1, 'fatal' => 1 };
+}
+
+1;
+# vim: set sw=4 et:
